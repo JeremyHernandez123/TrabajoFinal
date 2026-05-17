35e1a5833ade677660381604570ab4f5081fa822
Jeremy Hernández <jeremysamuelhernandez@paucasesnovescifp.cat>
2026-05-17 21:31:46 +0200

    feat: Funció carregar categories al formulari

--- js/form-tasca.js
+++ js/form-tasca.js
@@ -1,3 +1,27 @@
 
+import { getCategories } from "./storage.js";
+import { getTasques, guardarTasques } from "./storage.js";
+
+function carregarCategories() {
+    const seleccionar = document.getElementById("categoria");
+    if (!seleccionar) return;
+
+    const categories = getCategories();
+
+    seleccionar.innerHTML = "";
+
+    const opcioDefecte = document.createElement("option");
+    opcioDefecte.value = "";
+    opcioDefecte.textContent = "-- Selecciona una categoria --";
+    seleccionar.appendChild(opcioDefecte);
+
+    for (let i = 0; i < categories.length; i++) {
+        const cat = categories[i];
+        const opcio = document.createElement("option");
+        opcio.value = JSON.stringify(cat);
+        opcio.textContent = cat.nom;
+        seleccionar.appendChild(opcio);
+    }
+}
 
 
