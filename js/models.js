da7ee30f2d4c4be0fd9e3fdf4e1b961b590e0859
Jeremy Hernández <jeremysamuelhernandez@paucasesnovescifp.cat>
2026-05-17 16:20:26 +0200

    feat: Creació funció generar Id

--- js/models.js
+++ js/models.js
@@ -7,8 +7,8 @@ export class Categoria {
 }
  
 export class Tasca {
-    constructor(id, titol, descripcio, data, categoria, prioritat) {
-        this.id = id;
+    constructor(titol, descripcio, data, categoria, prioritat) {
+        this.id = generarId();
         this.titol = titol;
         this.descripcio = descripcio;
         this.data = data;
@@ -16,4 +16,25 @@ export class Tasca {
         this.prioritat = prioritat;
         this.realitzada = false;
     }
+}
+
+export function generarId() {
+    const tasques = getTasques();
+
+    if (tasques.length === 0) {
+        return "task-001";
+    }
+
+    let maxNum = 0;
+    for (let i = 0; i < tasques.length; i++) {
+        const parts = tasques[i].id.split("-");
+        const num = parseInt(parts[1]);
+        if (num > maxNum) {
+            maxNum = num;
+        }
+    }
+
+    const nouNum = maxNum + 1;
+    const numFinal = String(nouNum).padStart(3, "0");
+    return "task-" + numFinal;
 }
